9d77a44f21f3d31b628e9f1de8e3e9fcdbd54558
Selvedin <selvedinh@gmail.com>
2023-03-17 17:49:12 +0100

    extracting db variables to .env file

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=quiz',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=' . $_ENV["DB_NAME"],
+    'username' => $_ENV["DB_USER"],
+    'password' => $_ENV["DB_PASSWORD"],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
